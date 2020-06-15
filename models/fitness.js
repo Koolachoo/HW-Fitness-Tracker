270fe10b5ec1eb7efb4ef002051ae62b22be4f41
Justin Taylor <kiradez211@gmail.com>
2020-06-15 01:12:05 -0400

    updated routes

--- models/fitness.js
+++ models/fitness.js
@@ -2,8 +2,40 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var fitness = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true
+            },
+            name: {
+                type: String,
+                trim: true
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
+}
+);
 
-});
 
 const Fit = mongoose.model("Fit", fitness);
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,29 +1,29 @@
 const Fit = require("../models/fitness.js");
 module.exports = function (app) {
 
-app.get("/api/workouts", function (req, res) {
-    Fit.find()
-        .then(function (data) {
-            res.json(data)
-        })
-});
+    app.get("/api/workouts", function (req, res) {
+        Fit.find()
+            .then(function (data) {
+                res.json(data)
+            })
+    });
 
-app.post("/api/workouts", function (req, res) {
-    Fit.create({})
-        .then(function (data) {
-            res.json(data)
-        })
-});
+    app.post("/api/workouts", function (req, res) {
+        Fit.create({})
+            .then(function (data) {
+                res.json(data)
+            })
+    });
 
-app.put("/api/workouts/:id", function (req, res) {
-    Fit.findByIdAndUpdate(
-        params.id,
-        { $push: { exercises: body } },
-        { new: true, runValidators: true }
-    )
-        .then(function (data) {
-            res.json(data)
-        })
-});
+    app.put("/api/workouts/:id", function ({body, params}, res) {
+        Fit.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            { new: true, runValidators: true }
+        )
+            .then(function (data) {
+                res.json(data)
+            })
+    });
 
 }
